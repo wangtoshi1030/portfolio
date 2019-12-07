ddf033bf21af55cea8eb3ff5c60260ce810df8b0
橋本昌林 <wangtoshiwork1881030@icloud.com>
2019-12-07 23:25:59 +0900

    topページ作成

---
+++ js/app.js
@@ -0,0 +1,25 @@
+console.log("反応");
+
+$(function(){
+    // 画像が自動で切り替わる
+    $(".slick").slick({
+        infinite: true,
+    speed: 500,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    dots: false,
+    // cssEase: 'linear',
+    pauseOnHover: false,
+    })
+
+    //  押したらtopへ戻る
+    $(".fa-arrow-circle-up").click(function(){
+        console.log("clicked");
+        let name = $(".name").offset().top;
+        $("html,body").animate({scrollTop:name},1000);
+    })
+
+});
+
+
---
+++ php/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome-animation/0.2.1/font-awesome-animation.css">
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="summary-header">
+        <div>
+            <header class="name">Hashimoto Toshiki</header>
+        </div>
+        <ul class="list">
+            <li class="li"><a href="">Home</a></li>
+            <li class="li"><a href="https://qiita.com/wangtoshi">Qiita</a></li>
+            <li class="li"><a href="">GitHub</a></li>
+            <li class="li"><a href="./php/index.php">Portfolio</a></li>
+        </ul>
+    </div>
+</body>
+
+</html>
