709834ea5483d1a628b0b01a4a326dc422aa188c
橋本昌林 <wangtoshiwork1881030@icloud.com>
2019-12-09 13:41:03 +0900

    php作成

--- php/index.php
+++ php/index.php
@@ -7,23 +7,43 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome-animation/0.2.1/font-awesome-animation.css">
-    <link rel="stylesheet" href="reset.css">
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="../assets/css/php.css">
     <title>Document</title>
 </head>
 
 <body>
-    <div class="summary-header">
+    <div class="summary-header-php">
         <div>
-            <header class="name">Hashimoto Toshiki</header>
+            <header class="name-php">Hashimoto Toshiki</header>
         </div>
-        <ul class="list">
-            <li class="li"><a href="">Home</a></li>
-            <li class="li"><a href="https://qiita.com/wangtoshi">Qiita</a></li>
-            <li class="li"><a href="">GitHub</a></li>
-            <li class="li"><a href="./php/index.php">Portfolio</a></li>
+        <ul class="list-php">
+            <li class="li-php"><a href="../index.html">Home</a></li>
+            <li class="li-php"><a href="https://qiita.com/wangtoshi">Qiita</a></li>
+            <li class="li-php"><a href="">GitHub</a></li>
+            <li class="li-php"><a href="">Portfolio</a></li>
         </ul>
     </div>
+    <form action="" method="">
+        <div class="container-php">
+        <div class="imgPen">
+            <p class="parsonality">性格診断</p>
+        </div>
+        <div class="col-h"></div>
+        <div class="imgBook">
+            <p class="myself">あなた自身を知りましょう！！</p>
+        </div>
+        <div class="push">
+            <p class="start">Let's Start</p>
+        </div>
+        </div>
+    </form>
+    <footer class="footer-php">
+        <div class="last-php">Copyright©Toshiki.Hashimoto</div>
+        <i class=" arrow-php fas fa-arrow-circle-up"></i>
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="../js/php.js"></script>
 </body>
+
 
 </html>
